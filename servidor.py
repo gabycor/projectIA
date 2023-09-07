02c07d15dc267ddb7ce2c5ee6081a345cf23336d
gabycor <A01751655@tec.mx>
2023-09-06 19:19:00 -0600

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -19,7 +19,17 @@ def modeloPrediccion():
     #Procesar los datos de entrada
     contenido = request.json
     print(contenido)
-    return jsonify({'resultado':"Hola"})
+    datosEntrada = np.array([
+        0.88,0,2.6,0.098,25,67,0.9968,1,0.4,
+        contenido['pH'],
+        contenido(['sulphates']),
+        contenido['alcohol']
+    ])
+    #Utilizar el modelo
+    resultado = dt.predict(datosEntrada.reshape(1,-1))
+    return jsonify({'resultado':str(resultado[0])})
 
 if __name__ == '__main__':
     servidorWeb.run(debug=False, host = '0.0.0.0', port ='8080')
+
+    
